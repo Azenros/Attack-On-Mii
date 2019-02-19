e72aa9e81c1a1679b29a32454652dc2693d43bbd
Steven Tran <stran050@ucr.edu>
2019-02-18 22:04:37 -0800

    Uplaod project (final ver.)

---
+++ Scripts/jump.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public GameObject lHand;
+    public GameObject rHand;
+    public float movingUp = 0;
+    private float enable = 0;
+    private float upSpeed = 1f;
+    private float cooldown = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (movingUp > 3)
+        {
+            movingUp = 1;
+        }
+        
+        VRTK.VRTK_ControllerEvents l_hand = lHand.GetComponent<VRTK.VRTK_ControllerEvents>();
+        VRTK.VRTK_ControllerEvents r_hand = rHand.GetComponent<VRTK.VRTK_ControllerEvents>();
+
+        bool l_buttonOne = l_hand.IsButtonPressed(VRTK.VRTK_ControllerEvents.ButtonAlias.ButtonOnePress);
+        bool r_buttonOne = r_hand.IsButtonPressed(VRTK.VRTK_ControllerEvents.ButtonAlias.ButtonOnePress);
+        
+        if ((Input.GetKey("a") || l_buttonOne || r_buttonOne)  && (enable < 0.1)){
+            if (movingUp < 0)
+            {
+                movingUp = 2.1f ;
+            }
+            else
+            {
+                movingUp += 2.1f;
+                enable = 1;
+            }
+        }
+        
+        enable -= Time.deltaTime;
+        movingUp -= Time.deltaTime;
+        if (movingUp > 0.0){
+            if (transform.position.y < 6)
+            {
+                transform.position += Vector3.up * upSpeed * Time.deltaTime;
+            }
+            else
+            {
+                movingUp = 0;
+                enable = 3;
+            }
+        }
+        else{
+            if (transform.position.y > -5.5f)
+            {
+                movingUp = 0;
+                transform.position -= Vector3.up *  Time.deltaTime;
+            }
+        }
+    }
+}
+ 
---
+++ Scripts/titan_killable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titan_killable : MonoBehaviour
+{
+    public GameObject blade;
+    public GameObject rblade;
+    public GameObject titan;
+    public GameObject player;
+    public GameObject gameOverScreen;
+    public GameObject world;
+
+    public AudioClip hard_slash;
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = player.GetComponent<AudioSource>();
+        //hard_slash = player.GetComponent<AudioSource>().GetComponent<AudioClip>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collide");
+        if (blade.GetComponent<leftHand>().readBlade() && rblade.GetComponent<rightHand>().readBlade())
+        { //register a kill, make titan disappear, make blade disappear
+            
+            Debug.Log("Kill get");
+            blade.GetComponent<leftHand>().turnOffSword();
+            rblade.GetComponent<rightHand>().turnOffSword();
+            titan.SetActive(false);
+            source.Play();
+
+        }
+        else
+        { //teleport to game over screen and end play
+            Debug.Log("END");
+            gameOverScreen.SetActive(true);
+            player.GetComponent<gameStat>().inPlay = false;
+            player.transform.position = new Vector3(-125.8207f, -13.66788f, -4.377975f);
+            //source.PlayOneShot(soft_slash, 1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("t"))
+        {
+            player.GetComponent<gameStat>().inPlay = false;
+            player.transform.position = new Vector3(-125.8207f, -13.66788f, -4.377975f);
+        }
+    }
+}
